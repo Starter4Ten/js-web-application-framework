8f64d9d90943029cfe1640868b849aec541f107b
shem <shem@starter4ten.com>
2012-08-21 22:19:26 +1200

    basic test of signup page

--- src/test/web/com/starter4ten/SignupPage.java
+++ src/test/web/com/starter4ten/SignupPage.java
@@ -1,5 +1,50 @@
 package com.starter4ten;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
 public class SignupPage {
+	@FindBy(id = "email")
+	private WebElement emailField;
+
+	@FindBy(id = "name")
+	private WebElement nameField;
+
+	@FindBy(id = "colours")
+	private WebElement coloursSelection;
+	
+	@FindBy(id = "password")
+	private WebElement passwordField;
+
+	@FindBy(id = "passwordRpt")
+	private WebElement passwordRptField;
+	
+	@FindBy(id = "submit")
+	private WebElement submitButton;
+	
+	@FindBy(id = "reset")
+	private WebElement cancelButton;
+
+	public void assertAllElementsDisplayed() {
+		assertThat(emailField.isDisplayed(), is(true));
+		assertThat(nameField.isDisplayed(), is(true));
+		assertThat(coloursSelection.isDisplayed(), is(true));
+		assertThat(passwordField.isDisplayed(), is(true));
+		assertThat(passwordRptField.isDisplayed(), is(true));
+		assertThat(submitButton.isDisplayed(), is(true));
+		assertThat(cancelButton.isDisplayed(), is(true));		
+	}
+
+	public String getEmail() {
+		return emailField.getText();
+	}
+
+	public String getName() {
+		return nameField.getText();
+	}
 
 }
--- src/test/web/com/starter4ten/SignupTest.java
+++ src/test/web/com/starter4ten/SignupTest.java
@@ -2,14 +2,25 @@ package com.starter4ten;
 
 
 import org.junit.Test;
+import static org.hamcrest.text.IsEmptyString.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class SignupTest extends AbstractTestBase {
 
 	@Test
-	public void test() {
+	public void testSignupPageDisplaysUserElements_withBlankDefaults() {
+		// GIVEN we are on the registration page and the user clicks on signup links
 		 SignupPage signupPage = pages.currentPageAt(RegistrationPage.class).signup();
-		
-
+		 
+		// THEN signup page is shown with all expected elements
+		 signupPage.assertAllElementsDisplayed();
+		 
+		 // AND email is blank
+		 assertThat(signupPage.getEmail(), isEmptyString());
+		 
+		 // AND name is blank
+		 assertThat(signupPage.getEmail(), isEmptyString());
 	}
 
 }
